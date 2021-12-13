2e8fbecfc2060325072d03a4950f4d9ca9ad2866
Christopher Biscardi <chris@christopherbiscardi.com>
2021-12-13 00:04:29 -0800

    query pagination

--- src/bin/query_pagination.rs
+++ src/bin/query_pagination.rs
@@ -7,22 +7,42 @@ async fn main() -> Result<(), Error> {
     let shared_config = aws_config::load_from_env().await;
     let client = Client::new(&shared_config);
     let mut results = vec![];
+    let mut exclusive_start_key = None;
     loop {
         let req = client
-        .query()
-        .table_name("InfrastructureStack-CoursesTable3F79D98E-KX1JZVQZ2ANG")
-        .limit(1)
-        .set_exclusive_start_key(None)
-        .key_condition_expression("pk = :pk and begins_with(sk, :course)")
-        .expression_attribute_values(
-            ":pk",
-            AttributeValue::S("rust-adventure".to_string()),
-        )
-        .expression_attribute_values(":course", AttributeValue::S("course".to_string()))
-        .send()
-        .await?;
-        dbg!(req);
-        results.extend(req.items);
+            .query()
+            .table_name("my-table")
+            .limit(10)
+            .set_exclusive_start_key(exclusive_start_key)
+            .key_condition_expression(
+                "id = :hashKey and createdAt > :rangeKey",
+            )
+            .expression_attribute_values(
+                ":hashKey",
+                AttributeValue::S("123".to_string()),
+            )
+            .expression_attribute_values(
+                ":rangeKey",
+                AttributeValue::N(20150101.to_string()),
+            )
+            .send()
+            .await?;
+
+        if let Some(items) = req.items {
+            results.extend(items);
+            match req.last_evaluated_key {
+                Some(last_evaluated_key) => {
+                    exclusive_start_key =
+                        Some(last_evaluated_key.clone());
+                }
+                None => {
+                    break;
+                }
+            }
+        } else {
+            break;
+        }
     }
+    dbg!(results);
     Ok(())
 }
