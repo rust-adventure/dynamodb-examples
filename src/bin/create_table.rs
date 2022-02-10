6e615f7008f75ec38077c4f291d8c2020cd61e32
Christopher Biscardi <chris@christopherbiscardi.com>
2022-02-10 01:41:22 -0800

    finish

--- src/bin/create_table.rs
+++ src/bin/create_table.rs
@@ -1,25 +1,14 @@
-use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_dynamodb::{
-    error::{DescribeTableError, DescribeTableErrorKind},
     model::{
         AttributeDefinition, BillingMode, KeySchemaElement,
-        KeyType, ScalarAttributeType, TableDescription,
-        TableStatus,
+        KeyType, ScalarAttributeType,
     },
-    output::DescribeTableOutput,
-    Client, Error, Region,
+    Client, Error,
 };
-use tokio::time;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let region_provider =
-        RegionProviderChain::default_provider()
-            .or_else(Region::new("us-west-2"));
-    let shared_config = aws_config::from_env()
-        .region(region_provider)
-        .load()
-        .await;
+    let shared_config = aws_config::load_from_env().await;
     let client = Client::new(&shared_config);
 
     let key = "name";
@@ -42,46 +31,5 @@ async fn main() -> Result<(), Error> {
         .send()
         .await?;
 
-    let mut interval =
-        time::interval(time::Duration::from_secs(2));
-
-    loop {
-        println!(
-            "Waiting for database {} to become available",
-            "my-table"
-        );
-        interval.tick().await;
-        let resp = client
-            .describe_table()
-            .table_name("my-table")
-            .send()
-            .await;
-        match resp {
-            Ok(DescribeTableOutput {
-                table:
-                    Some(TableDescription {
-                        table_status: Some(TableStatus::Active),
-                        ..
-                    }),
-                ..
-            }) => {
-                break;
-            }
-            Err(aws_sdk_dynamodb::SdkError::ServiceError {
-                err:
-                    DescribeTableError {
-                        kind: DescribeTableErrorKind::ResourceNotFoundException(_),
-                        ..
-                    },
-                raw: _,
-            }) => {
-                // keep looping, the database metadata may not be populated yet.
-            }
-            e => {
-                // keep looping if it's not an error, otherwise return
-                e?;
-            }
-        }
-    }
     Ok(())
 }
